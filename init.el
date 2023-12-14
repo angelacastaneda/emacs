693b35a89e66fa561259327707eb8bb741e350ed
Ángel Castañeda <angel@acsq.me>
2023-12-14 05:15:07 -0600

    Save config generated by use-package

--- init.el
+++ init.el
@@ -18,3 +18,10 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key use-package bind-key)))
