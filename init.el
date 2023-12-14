4cf7ef6d8c98553539675609c0b49aa13b631baf
Ángel Castañeda <angel@acsq.me>
2023-12-14 05:23:48 -0600

    Add gruvbox theme

--- init.el
+++ init.el
@@ -24,7 +24,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key use-package bind-key)))
+ '(custom-enabled-themes '(gruvbox-dark-hard))
+ '(custom-safe-themes
+   '("d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" default))
+ '(package-selected-packages '(gruvbox-theme which-key use-package bind-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
