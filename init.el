17e5ef4b9ee89abf27cf461241da032727659fbe
Ángel Castañeda <angel@acsq.me>
2023-12-29 16:55:48 -0600

    Remove gruvbox theme

--- init.el
+++ init.el
@@ -9,11 +9,6 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; require gruvbox-theme
-(unless (package-installed-p 'gruvbox-theme)
-  (package-refresh-contents)
-  (package-install 'gruvbox-theme))
-
 (org-babel-load-file (expand-file-name "~/.config/emacs/config.org"))
 
 (custom-set-variables
@@ -21,11 +16,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruvbox-dark-hard))
- '(custom-safe-themes
-   '("d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" default))
  '(package-selected-packages
-   '(vertico marginalia org-roam magit diminish dashboard rainbow-delimiters rainbow-mode avy ido-vertical-mode smex evil-collection evil undo-fu gruvbox-theme which-key use-package bind-key)))
+   '(vertico marginalia org-roam magit diminish dashboard rainbow-delimiters rainbow-mode avy ido-vertical-mode smex evil-collection evil undo-fu which-key use-package bind-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
