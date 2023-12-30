6d6bcd6b40252e10a92be8f0b7b850861f939b13
Ángel Castañeda <angel@acsq.me>
2023-12-30 01:45:10 -0600

    Change git gutter to VS Code style line
    Got from this article: ianyepan.github.io/posts/emacs-git-gutter/

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(git-gutter vertico marginalia org-roam magit diminish dashboard rainbow-delimiters rainbow-mode avy ido-vertical-mode smex evil-collection evil undo-fu which-key use-package bind-key)))
+   '(git-gutter-fringe git-gutter vertico marginalia org-roam magit diminish dashboard rainbow-delimiters rainbow-mode avy ido-vertical-mode smex evil-collection evil undo-fu which-key use-package bind-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
