ae318b2c30062d729da1fb5674596dd646ce8906
Ángel Castañeda <angel@acsq.me>
2024-07-13 21:28:22 -0500

    org: Remove size scaling from org headers

--- init.el
+++ init.el
@@ -37,13 +37,3 @@
      (:name "to-delete" :query "tag:deleted")))
  '(package-selected-packages
    '(racket-mode sicp orderless git-gutter-fringe git-gutter vertico marginalia org-roam magit diminish dashboard rainbow-delimiters rainbow-mode avy ido-vertical-mode smex undo-fu which-key use-package bind-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.8))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.6))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
